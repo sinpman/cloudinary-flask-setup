79930387d9750d2d23b1252c4df82d52fa7986e2
Swastik <sinpman@outlook.com>
2019-05-03 03:28:25 +0000

    Test cloudinary upload

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 
+import cloudinary
+
 app = Flask(__name__)
 
 
@@ -7,3 +9,7 @@ app = Flask(__name__)
 def index():
     if request.method == "GET":
         return render_template("index.html")
+    if request.method == "POST":
+        uploaded_file = cloudinary.uploader.upload(request.files['image_file'])
+        image_url = cloudinary.utils.cloudinary_url(uploaded_file['public_id'], format="jpg", width="100", height="100")
+        return render_template("success.html", image_url=image_url)
