3a150ce7c3da4847f800181798fad06fac5aa7f3
Swastik <sinpman@outlook.com>
2019-05-03 03:31:54 +0000

    Fix cloudinary import statements, fix image_url

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
-import cloudinary
+from cloudinary.uploader import upload
+from cloudinary.utils import cloudinary_url
 
 app = Flask(__name__)
 
@@ -10,6 +11,6 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     if request.method == "POST":
-        uploaded_file = cloudinary.uploader.upload(request.files['image_file'])
-        image_url = cloudinary.utils.cloudinary_url(uploaded_file['public_id'], format="jpg", width="100", height="100")
+        uploaded_file = upload(request.files['image_file'])
+        image_url, options = cloudinary_url(uploaded_file['public_id'], format="jpg", width="100", height="100")
         return render_template("success.html", image_url=image_url)
